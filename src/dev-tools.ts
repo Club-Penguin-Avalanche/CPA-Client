2ec53ab83c60a84a4bb430d8878e76576b3b45af
Renan Pinheiro <renancamerapinheiro@gmail.com>
2023-02-17 23:39:43 -0300

    feat: add confirmation when opening dev tools

--- src/dev-tools.ts
+++ src/dev-tools.ts
@@ -1,6 +1,16 @@
-import { BrowserWindow } from "electron";
+import { BrowserWindow, dialog } from "electron";
 
-const openDevTools = (mainWindow: BrowserWindow) => {
+const openDevTools = async (mainWindow: BrowserWindow) => {
+  const confirmationResult = await dialog.showMessageBox(mainWindow, {
+    buttons: ['Yes', 'No', 'Cancel'],
+    title: 'Do you really want to open the Dev Tools?',
+    message: `The Dev Tools can contains sensitive information about your game session.`,
+  });
+
+  if (confirmationResult.response !== 0) {
+    return;
+  }
+  
   mainWindow.webContents.openDevTools();
 };
 

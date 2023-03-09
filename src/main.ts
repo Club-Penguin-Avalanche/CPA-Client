964842856f935dfaa49e1ebe00aec535fa4fda5c
Renan Pinheiro <renancamerapinheiro@gmail.com>
2023-03-09 18:45:48 -0300

    feat: add cpavalanche protocol

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,8 @@ app.on('ready', async () => {
   });
 });
 
+app.setAsDefaultProtocolClient("cpavalanche");
+
 app.on('window-all-closed', async () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q

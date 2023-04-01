9cc59e88b39b8d446966453f0b6af7c901d4a527
Arthur Eller <arthurmachadoeller@hotmail.com>
2023-04-01 02:57:43 -0300

    fix client crashing on macOS (#1)
    Co-authored-by: Arthur Eller <arthur.eller@m23.studio>

--- src/window.ts
+++ src/window.ts
@@ -1,49 +1,62 @@
 import { BrowserWindow } from "electron";
 import path = require("path");
 import { createAdblocker } from "./adblocker";
-import { checkIfLoadedFromProtocol, getUrlFromCommandLineProcess } from "./protocol";
+import {
+  checkIfLoadedFromProtocol,
+  getUrlFromCommandLineProcess,
+} from "./protocol";
 import { Store } from "./store";
 import { getUrlFromStore } from "./urlchanger";
 
-
 export const toggleFullScreen = (store: Store, mainWindow: BrowserWindow) => {
-  const fullScreen = !store.private.get('fullScreen') ?? false;
+  const fullScreen = !store.private.get("fullScreen") ?? false;
 
-  store.private.set('fullScreen', fullScreen);
+  store.private.set("fullScreen", fullScreen);
 
   mainWindow.setFullScreen(fullScreen);
 };
 
+interface FiveIconByPlatforms {
+  [key: string]: () => void;
+}
+
 const createWindow = async (store: Store) => {
+  const setFaviconByPlatform: FiveIconByPlatforms = {
+    win32: () => {
+      mainWindow.setIcon(path.join(__dirname, "assets/favicon.ico"));
+    },
+    darwin: () => {
+      mainWindow.setIcon(path.join(__dirname, "assets/icon.png"));
+    },
+    linux: () => {
+      mainWindow.setIcon(path.join(__dirname, "assets/icon.png"));
+    },
+  };
+
   const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
     title: "Iniciando...",
     webPreferences: {
-      plugins: true
-    }
+      plugins: true,
+    },
   });
 
-  if (process.platform === 'linux') {
-    mainWindow.setIcon(path.join(__dirname, 'assets/icon.png'));
-  } else {
-    mainWindow.setIcon(path.join(__dirname, 'assets/favicon.ico'));
-  }
-  
+  setFaviconByPlatform[process.platform]();
 
   mainWindow.setMenu(null);
   mainWindow.maximize();
 
   await createAdblocker(store, mainWindow);
 
-  let url = '';
+  let url = "";
 
   if (checkIfLoadedFromProtocol()) {
     url = getUrlFromCommandLineProcess();
   } else {
     url = getUrlFromStore(store);
   }
-  
+
   mainWindow.loadURL(url);
 
   return mainWindow;
